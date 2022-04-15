80206aeb37378da949bb763cc442858eac3398e5
Kieran <Kiki@MacBook-Pro.local>
2022-04-15 09:17:09 -0400

    a9 MONGO sp22

---
+++ src/components/labs/todo/todo-item 6.js
@@ -0,0 +1,36 @@
+// const TodoItem = (todo) => {
+//     return(`
+//         <li>${todo}</li>
+//     `);
+// }
+// export default TodoItem;
+
+// const TodoItem = (todo) => {
+//     return(`
+//         <li>
+//             ${todo.title}
+//             (${todo.status})
+//         </li>
+//     `);
+// }
+// export default TodoItem;
+
+const TodoItem = ({
+                      todo = {
+                          done: true,
+                          title: 'Buy milk',
+                          status: 'COMPLETED'
+                      }
+                  }) => {
+    return(
+        <li>
+            <input type="checkbox"
+                   defaultChecked={todo.done}/>
+            {todo.title}
+            ({todo.status})
+        </li>
+    );
+}
+export default TodoItem;
+
+
